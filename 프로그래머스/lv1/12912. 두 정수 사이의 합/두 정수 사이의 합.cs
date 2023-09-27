adf36be422f775a4a57870b7afc89f7f52d450c4
coco0715 <101281567+coco0715@users.noreply.github.com>
2023-09-27 09:48:36 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.17 ms, Memory: 31.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12912. 두 정수 사이의 합/두 정수 사이의 합.cs
@@ -0,0 +1,23 @@
+public class Solution {
+    public long solution(int a, int b) {
+        long answer = 0;
+        
+        if (a < b)
+        {
+            for(int i = a; i <= b; i++){
+                answer += (long)i;
+            }    
+        }
+        else if (a >b)
+        {
+            for(int i = a; i >= b; i--){
+                   answer += (long)i;
+                }    
+        }
+        else 
+        {
+            answer = a;
+        }
+        return answer;
+    }
+}
